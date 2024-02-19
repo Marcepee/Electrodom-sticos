9a934c66717e150793b34c4fcc826f4a3d7dfd24
Marcepee <marcepe@hotmail.nl>
2024-02-19 10:41:39 +0300

    First

---
+++ src/Electrodomestico.java
@@ -0,0 +1,32 @@
+public class Electrodomestico {
+    private String tipo;
+    private String marca;
+    private double potencia;
+    public Electrodomestico(String tipo, String marca, double potencia){
+
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(double potencia) {
+        this.potencia = potencia;
+    }
+}
